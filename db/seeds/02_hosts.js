44f2de7f50199f1038e3f5d430330317477f8c1b
Bryan Kai <bkai@apptio.com>
2022-07-23 21:29:34 -0700

    replaced some of the broken image URLs

--- db/seeds/02_hosts.js
+++ db/seeds/02_hosts.js
@@ -6,11 +6,11 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex(TABLE_NAME).insert([
-        {id: 1, first_name: 'Ariel', last_name: 'Anderson', email: 'ariel@gmail.com', hashed_password: '$2a$10$vmSqInKADj2nT7iy2C1z9.uggyjKcodbPgCFYyerIGFHwgyxnnlfe', avatar: 'https://media.licdn.com/dms/image/C5603AQG9ptrSLXSIog/profile-displayphoto-shrink_800_800/0?e=1536192000&v=beta&t=1_xWVWIt3ezSwtp0GkD0QdT79OFdOJtUbGCDzYTo-XI'},
-        {id: 2, first_name: 'Bob', last_name: 'Bolt', email: 'bob@gmail.com', hashed_password: '$2a$10$vmSqInKADj2nT7iy2C1z9.uggyjKcodbPgCFYyerIGFHwgyxnnlfe', avatar: 'https://a0.muscache.com/im/pictures/user/ca134f6b-59e0-4e44-9c42-6b14ada1a07d.jpg?aki_policy=profile_x_medium'},
-        {id: 3, first_name: 'Cathleen', last_name: 'Chase', email: 'cathleen@gmail.com', hashed_password: '$2a$10$vmSqInKADj2nT7iy2C1z9.uggyjKcodbPgCFYyerIGFHwgyxnnlfe', avatar: 'https://a0.muscache.com/im/pictures/454744fa-f630-4200-a466-0ea0e7cd8ae9.jpg?aki_policy=profile_x_medium'},
-        {id: 4, first_name: 'Daniel', last_name: 'Doppler', email: 'daniel@gmail.com', hashed_password: '$2a$10$vmSqInKADj2nT7iy2C1z9.uggyjKcodbPgCFYyerIGFHwgyxnnlfe', avatar: 'https://a0.muscache.com/im/users/26967583/profile_pic/1427823555/original.jpg?aki_policy=profile_x_medium'},
-        {id: 5, first_name: 'Emily', last_name: 'Everett', email: 'emily@gmail.com', hashed_password: '$2a$10$vmSqInKADj2nT7iy2C1z9.uggyjKcodbPgCFYyerIGFHwgyxnnlfe', avatar: 'https://a0.muscache.com/im/pictures/489fca10-66b4-4baa-a692-4744d49d810a.jpg?aki_policy=profile_x_medium'},
+        {id: 1, first_name: 'Ariel', last_name: 'Anderson', email: 'ariel@gmail.com', hashed_password: '$2a$10$vmSqInKADj2nT7iy2C1z9.uggyjKcodbPgCFYyerIGFHwgyxnnlfe', avatar: 'https://images.unsplash.com/photo-1460661419201-fd4cecdf8a8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'},
+        {id: 2, first_name: 'Bob', last_name: 'Bolt', email: 'bob@gmail.com', hashed_password: '$2a$10$vmSqInKADj2nT7iy2C1z9.uggyjKcodbPgCFYyerIGFHwgyxnnlfe', avatar: 'https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'},
+        {id: 3, first_name: 'Cathleen', last_name: 'Chase', email: 'cathleen@gmail.com', hashed_password: '$2a$10$vmSqInKADj2nT7iy2C1z9.uggyjKcodbPgCFYyerIGFHwgyxnnlfe', avatar: 'https://images.unsplash.com/photo-1522039553440-46d3e1e61e4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1478&q=80'},
+        {id: 4, first_name: 'Daniel', last_name: 'Doppler', email: 'daniel@gmail.com', hashed_password: '$2a$10$vmSqInKADj2nT7iy2C1z9.uggyjKcodbPgCFYyerIGFHwgyxnnlfe', avatar: 'https://images.unsplash.com/photo-1529903543134-d2d0b6858e21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'},
+        {id: 5, first_name: 'Emily', last_name: 'Everett', email: 'emily@gmail.com', hashed_password: '$2a$10$vmSqInKADj2nT7iy2C1z9.uggyjKcodbPgCFYyerIGFHwgyxnnlfe', avatar: 'https://images.unsplash.com/photo-1566847438217-76e82d383f84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'},
       ]);
     })
     .then(() => {
